9b2a2f9d04b8c734919767d6b834d4af830d21de
ssynhtn <huangtongnao@gmail.com>
2018-06-18 12:36:47 +0800

    test dog

---
+++ src/Hello/Dog.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ssynhtn
+ * Date: 2018/6/18
+ * Time: 12:36
+ */
+namespace Hello;
+
+class Dog {
+    function bark() {
+        echo "dog barking";
+    }
+}
