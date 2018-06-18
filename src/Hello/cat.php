6ba824a25511029685afcb846c89d7560042b424
ssynhtn <huangtongnao@gmail.com>
2018-06-18 12:42:53 +0800

    add cat, dog was good

---
+++ src/Hello/cat.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ssynhtn
+ * Date: 2018/6/18
+ * Time: 12:42
+ */
+namespace Hello;
+
+class Cat {
+    function meow() {
+        echo "cat meowing";
+    }
+}
